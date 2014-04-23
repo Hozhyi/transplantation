1c8fa7b85361a1402ced6cb69577abe0f774644d
jeenser <jeenser@gmail.com>
2014-04-24 00:49:06 +0300

    Update Organs.java

--- src/Organs.java
+++ src/Organs.java
@@ -1,14 +1,8 @@
 import java.util.ArrayList;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
 import javax.swing.JOptionPane;
 import java.io.Serializable;
+
 public class Organs implements Serializable  {
 	
 	
